169279c95e61f03bd6ce0a2e06dfe7f26231edf1
Lakshay Bansal <lakshay012003@gmail.com>
2024-07-21 03:20:41 +0530

    first commit

---
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -0,0 +1,19 @@
+import {Menu, Sidebar} from "lucide-react";
+import {
+    Sheet,
+    SheetTrigger,
+    SheetContent,
+} from "@/components/ui/sheet"
+
+export const MobileSidebar = () => {
+    return (
+        <Sheet>
+            <SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition">
+                <Menu />
+            </SheetTrigger>
+            <SheetContent side="left" className="p-0 bg-white">
+                <Sidebar/>
+            </SheetContent>
+        </Sheet>
+    )
+}
